1c754bbff1d2ba08a25ae939373ca528247d3e76
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-06-18 23:57:42 +0530

    upcoming_stream_integration

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+"use client";
+import Loader from '@/components/Loader';
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import { useGetCallById } from '@/hooks/useGetCallById';
+import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk"
+import { useState } from 'react';
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+    const { user, isLoaded } = useUser()
+    const [isSetUpComplete, setisSetUpComplete] = useState(false)
+    const { call, isCallLoading } = useGetCallById(id)
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+    if (!isLoaded || isCallLoading) return <Loader />
+
     return (
-        <div>
-            Meeting room : #{params.id}
-        </div>
+        <main className='h-screen w-full'>
+            <StreamCall call={call}>
+                <StreamTheme>
+                    {!isSetUpComplete ? (<MeetingSetup setisSetUpComplete={setisSetUpComplete} />) : (<MeetingRoom />)}
+                </StreamTheme>
+            </StreamCall>
+        </main>
     )
 }
 
---
+++ components/EndCallButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useCall, useCallStateHooks } from '@stream-io/video-react-sdk'
+import { Button } from './ui/button';
+import { useRouter } from 'next/navigation';
+const EndCallButton = () => {
+    const call = useCall()
+    const router = useRouter()
+    const { useLocalParticipant } = useCallStateHooks();
+    const localParticipant = useLocalParticipant();
+
+    const isMeetingOwner = localParticipant && call?.state.createdBy && localParticipant.userId === call?.state.createdBy.id
+
+    if (!isMeetingOwner) return null;
+
+    return (
+        <div>
+            <Button className='bg-red-500'
+                onClick={async () => {
+                    await call.endCall()
+                    router.push('/')
+                }}>End Call for everyone</Button>
+        </div>
+    )
+}
+
+export default EndCallButton
